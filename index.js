962df983dbbc0bfdb67a0f0c068bac87139dce7b
Divy Agrawal <divyagrawal29@gmail.com>
2023-11-09 02:35:17 +0530

    Implemented functionality

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+import express from "express";
+import axios from "axios";
 
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}/random`);
+    res.render("index.ejs", {
+      secret: response.data.secret,
+      user: response.data.username,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
